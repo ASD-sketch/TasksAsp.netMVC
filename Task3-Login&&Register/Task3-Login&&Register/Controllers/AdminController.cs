7a7495633497dc83ca950234d6859a1ef6742fa4
ASD-sketch <ssyahmads@gmail.com>
2025-02-20 20:32:37 +0300

    EditTask4-20-2-2025

---
+++ Task3-Login&&Register/Task3-Login&&Register/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task3_Login__Register.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminHome()
+        {
+           
+            return View();
+        }
+
+
+        public IActionResult Profile()
+        {
+            String userName = HttpContext.Session.GetString("UserName");
+            String PassWord = HttpContext.Session.GetString("Password");
+
+            if (userName != null && PassWord != null)
+            {
+                ViewBag.UserName = HttpContext.Session.GetString("UserName");
+                ViewBag.Password = HttpContext.Session.GetString("Password");
+            }
+
+            return View();
+        }
+
+
+    }
+}
--- Task3-Login&&Register/Task3-Login&&Register/Controllers/UserController.cs
+++ Task3-Login&&Register/Task3-Login&&Register/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics.Arm;
 
 namespace Task3_Login__Register.Controllers
 {
@@ -15,10 +16,43 @@ namespace Task3_Login__Register.Controllers
         [HttpPost]
         public IActionResult Register(string username, string password)
         {
-            HttpContext.Session.SetString("UserName", username);
-            HttpContext.Session.SetString("Password", password);
+
+            //HttpContext.Session.SetString("UserName", username);
+            //HttpContext.Session.SetString("Password", password);
+            CookieOptions cook = new CookieOptions();
+            cook.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Append("UserName", username, cook);
+            Response.Cookies.Append("Password", password, cook);
             return RedirectToAction("Login");
         }
+
+
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string username, string password)
+        {
+
+            //HttpContext.Session.SetString("UserName", username);
+            //HttpContext.Session.SetString("Password", password);
+            CookieOptions cook = new CookieOptions();
+            cook.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Append("UserName", username, cook);
+            Response.Cookies.Append("Password", password, cook);
+
+            HttpContext.Session.SetString("UserName", username);
+            HttpContext.Session.SetString("Password", password);
+
+            return RedirectToAction("Profile");
+        }
+
+
+
+
 
 
         public IActionResult Login()
@@ -27,29 +61,77 @@ namespace Task3_Login__Register.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(string username, string password)
+        public IActionResult Login(string username, string password, string remember)
         {
-            String userName =  HttpContext.Session.GetString("UserName");
-            String PassWord = HttpContext.Session.GetString("Password");
+            //HttpContext.Session.SetString("UserName", username);
+            //HttpContext.Session.SetString("Password", password);
+            //String userName = HttpContext.Session.GetString("UserName");
+            //String PassWord = HttpContext.Session.GetString("Password");
 
 
-            if (username == userName && password == PassWord) // مثال على تحقق بسيط
+            if (username == "admin" && password == "1234") 
+            {
+                HttpContext.Session.SetString("UserName", username);
+                HttpContext.Session.SetString("Password", password);
+                return RedirectToAction("AdminHome", "Admin");
+            }
+
+
+
+            string userNamCookies = Request.Cookies["UserName"];
+            string PassWordCookies = Request.Cookies["Password"];
+
+            if (username == userNamCookies && password == PassWordCookies) 
+            {
+                if (remember == null)
+                {
+                    HttpContext.Session.SetString("UserName", username);
+                    HttpContext.Session.SetString("Password", password);
+                    return RedirectToAction("Home", "Home");
+                }
+                if (remember != null) 
+                {
+                    CookieOptions cook = new CookieOptions();
+                    cook.Expires = DateTime.Now.AddDays(1);
+                    Response.Cookies.Append("UserName", username, cook);
+                    Response.Cookies.Append("Password", password, cook);
+                    return RedirectToAction("Home", "Home");
+                }
+            }
+
+            if(username== userNamCookies && password== PassWordCookies)
             {
                 HttpContext.Session.SetString("UserName", username);
                 return RedirectToAction("Home", "Home");
             }
-            ViewBag.Error = "اسم المستخدم أو كلمة المرور غير صحيحة";
+
+            ViewBag.Error = "Username or Password not correct";
             return View();
         }
 
 
-        public IActionResult Profile() 
+        public IActionResult Profile()
         {
-            ViewBag.UserName = HttpContext.Session.GetString("UserName");
-            ViewBag.Password = HttpContext.Session.GetString("Password");
-            return View();
-            
+            String userName = HttpContext.Session.GetString("UserName");
+            String PassWord = HttpContext.Session.GetString("Password");
+
+            if (userName!=null && PassWord !=null)
+            {
+                ViewBag.UserName = HttpContext.Session.GetString("UserName");
+                ViewBag.Password = HttpContext.Session.GetString("Password");
+            }
+
+            string userNamCookies = Request.Cookies["UserName"];
+            string PassWordCookies = Request.Cookies["Password"];
+
+            if (userNamCookies !=null && PassWordCookies !=null)
+            {
+                ViewBag.UserName1 = Request.Cookies["UserName"];
+                ViewBag.Password1 = Request.Cookies["Password"];
+            }
+
            
+            return View();
         }
 
         public IActionResult Logout()
